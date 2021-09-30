680436cdd830e62983fd9587ea2bd03da7b495a6
Timur Kurmaev <timur95trigan@yandex.ru>
2021-09-30 19:36:35 +0300

    Removed prevSum

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
-let prevSum = 0;
-
-let sum = function(a){
-  if (!a) return prevSum;
+const sum = function(a){
   return function(b){
-    if(b) {
-      prevSum = a+b;
-      return sum(a+b);
-    }
+      if(b) {
+        return sum(a+b);
+      }
     return a;
     }
 } 
 
 console.log(sum(1)(2)(3)(4)()); //10
-console.log(sum()); //10
 console.log(sum(1)(2)(3)(5)()); //11
-console.log(sum()); //11
